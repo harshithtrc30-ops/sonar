8b9bfadf0ac2592670425084a234ddc7b3909345
harshithtrc30-ops <harshithtrc30@gmail.com>
2026-06-01 09:55:42 +0530

    Create sonarr.java

---
+++ sonarr.java
@@ -0,0 +1,20 @@
+public class datatypes{
+    public static void main(String[]rgs){
+        byte b=100;
+        short s=200;
+        int num=1331;
+        long l=923746581
+        double n1=22.4;
+        float n2=22.4f;
+
+        system.out.println(b);
+        System.out.println(s);
+        System.out.println(num);
+        System.out.println(l);
+        System.out.println(n1);
+        System.out.println(n2);
+        
+    
+}
+
+    
